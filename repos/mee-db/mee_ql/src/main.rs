dfd4a385a4e232cddd83ff3194b7188acd3be7a8
matveevAnton <140398722+matveevAnton@users.noreply.github.com>
2024-10-15 13:32:50 +0400

    clean main

--- repos/mee-db/mee_ql/src/main.rs
+++ repos/mee-db/mee_ql/src/main.rs
@@ -7,8 +7,6 @@ mod support;
 // mod visitor;
 // mod _visitor;
 
-// Импортируем сгенерированный модуль языка, например, `tree_sitter_mee_ql`
-
 fn main() {
     let source_code = r#"
        [
@@ -21,22 +19,9 @@ fn main() {
     match builder.parse() {
         Ok(ast) => {
             println!("Parsed AST: {:#?}", ast);
-            // Далее вы можете использовать посетителей для обработки AST
         }
         Err(e) => {
             eprintln!("Error parsing source code: {}", e);
         }
     }
-
-    // // Парсим код и получаем синтаксическое дерево
-    // let tree = parser
-    //     .parse(source_code, None)
-    //     .expect("Failed to parse code");
-    // let root_node = tree.root_node();
-    // println!("{:#?}", root_node.to_sexp());
-
-    // let mut visitor = Visitor::new();
-
-    // let ast = visitor.visit(root_node, source_code);
-    // println!("{:#?}", ast);
 }
